4db46116d660790550af0be32468858d321609d3
Oluakinwale03 <oluakinwale03@gmail.com>
2023-01-25 22:01:30 -0500

    My first commit

---
+++ ASCIIExample.java
@@ -0,0 +1,18 @@
+package Week1;
+
+public class ASCIIExample  {
+
+	public static void main(String[] args) {
+//		char a = 'A';
+//		int b = a;
+//		System.out.println("The ASCII value of " + a + " is: " + b);
+		
+		int count = 1;
+		while (count <= 10) {
+		    count *= 2;
+		}
+		count = count - 10;
+		System.out.print(count);
+	}
+
+}
---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+package Week1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(4 - 3);
+ 
+	}
+
+}
